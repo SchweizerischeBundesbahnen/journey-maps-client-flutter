7793982deda1e1ab37d354ad6c40ad4e8da4f865
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-07-08 17:05:41 +0200

    fix: kotlin dsl syntax migration issues

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,36 +10,16 @@ plugins {
     id("maven-publish")
 }
 
-val localProperties = java.util.Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.inputStream().use { input ->
-        localProperties.load(input)
-    }
-}
-
-var flutterVersionCode: String? = localProperties.getProperty("flutter.versionCode")
-if (flutterVersionCode == null) {
-    flutterVersionCode = "1"
-}
-
-var flutterVersionName: String? = localProperties.getProperty("flutter.versionName")
-if (flutterVersionName == null) {
-    flutterVersionName = "1.0"
-}
-
 // for local signing
-val keystoreProperties = java.util.Properties()
+val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-    keystorePropertiesFile.inputStream().use { input ->
-        keystoreProperties.load(input)
-    }
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
     namespace = "ch.sbb.maps.flutter"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.1.13356709" // required by geolocator_android & maplibre_gl
 
     compileOptions {
@@ -43,22 +27,12 @@ android {
         targetCompatibility = JavaVersion.VERSION_21
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_21.toString()
-    }
-
-    sourceSets {
-        getByName("main") {
-            java.srcDir("src/main/kotlin")
-        }
-    }
-
     defaultConfig {
         applicationId = "ch.sbb.maps.flutter.example"
-        minSdk = 28
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutterVersionCode!!.toInt()
-        versionName = flutterVersionName
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     signingConfigs {
@@ -69,29 +43,27 @@ android {
             )
             storePassword = System.getenv("SIGNING_KEYSTORE_PASSWORD")
                 ?: keystoreProperties.getProperty("storePassword")
-            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
-                ?: keystoreProperties.getProperty("keyAlias")
+            keyAlias =
+                System.getenv("SIGNING_KEY_ALIAS") ?: keystoreProperties.getProperty("keyAlias")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
                 ?: keystoreProperties.getProperty("keyPassword")
         }
     }
 
     buildTypes {
-        getByName("debug")
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+}
 
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
-configure<com.github.triplet.gradle.androidpublisher.AndroidPublisherExtension> {
+play {
     serviceAccountCredentials.set(file("keys/google_playstore_service_account.json"))
     track.set("alpha")
     defaultToAppBundles.set(true)
@@ -101,15 +73,15 @@ configure<com.github.triplet.gradle.androidpublisher.AndroidPublisherExtension> 
 publishing {
     publications {
         android.applicationVariants.all {
-            val flavorName = this.flavorName
             this.outputs.forEach { output ->
                 val publicationName = output.outputFile.name.replace(".apk", "")
                 create<MavenPublication>(publicationName) {
-                    val path = "${project.buildDir.absolutePath}/outputs/bundle/release/${publicationName}.aab"
+                    val path =
+                        "${project.layout.buildDirectory.get().asFile.absolutePath}/outputs/bundle/release/${publicationName}.aab"
                     artifact(File(path))
                     groupId = "ch.sbb.rokas.flutter"
                     artifactId = "ch.sbb.maps.flutter.example"
-                    version = flutterVersionName!!
+                    version = flutter.versionName
                 }
             }
         }
@@ -117,7 +89,7 @@ publishing {
     repositories {
         maven {
             name = "temp"
-            url = uri("${rootProject.buildDir}")
+            url = uri(rootProject.layout.buildDirectory.get().asFile)
         }
     }
 }
