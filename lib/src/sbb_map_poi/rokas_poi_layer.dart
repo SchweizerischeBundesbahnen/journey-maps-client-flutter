12390116d71a97e10f6b2d202f3c9d8c6bf2abfc
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-09-21 13:11:07 +0200

    docs: add new docs to poi layer

--- lib/src/sbb_map_poi/rokas_poi_layer.dart
+++ lib/src/sbb_map_poi/rokas_poi_layer.dart
@@ -1,6 +1,9 @@
 import 'package:sbb_maps_flutter/sbb_maps_flutter.dart';
 
-/// The POI layers defined in the [SBBRokasMapStyler] styles.
+/// The POI layers defined in the SBB map styles.
+///
+/// Identical in the styles held by [SBBMapsMapStyler] and by the deprecated
+/// [SBBRokasMapStyler].
 ///
 /// * [baseWithFloor] are circles / rounded squares icons shown in the 'background' on the map.
 ///   They are dependent on the currently selected floor.
